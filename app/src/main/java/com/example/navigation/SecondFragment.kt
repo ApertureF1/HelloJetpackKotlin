baf93792e9254839a3de194eca189b81715fa48b
jiali <jiali@telenav.cn>
2019-07-16 19:00:04 +0800

    remove unused code

--- app/src/main/java/com/example/navigation/SecondFragment.kt
+++ app/src/main/java/com/example/navigation/SecondFragment.kt
@@ -8,33 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SecondFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
  */
 class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i("test", "secondFragment onCreate()")
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -93,25 +74,5 @@ class SecondFragment : Fragment() {
     override fun onDetach() {
         Log.i("test", "secondFragment onDetach()")
         super.onDetach()
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
